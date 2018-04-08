a0a7e92fd664f5382f54e09d6c033191b14afec2
brokeandfurious <forumbomb@gmail.com>
2018-04-09 00:01:11 +0300

    test

--- FirstLottieTest/ViewController.swift
+++ FirstLottieTest/ViewController.swift
@@ -7,19 +7,32 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func playAnimation(_ sender: Any) {
+        print("Animation playing.")
+        
+        let animationView = LOTAnimationView.init(name: "LottieTest3")
+        animationView.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 250)
+        animationView.contentMode = .scaleAspectFill
+        animationView.loopAnimation = true
+        
+        self.view.addSubview(animationView)
+        animationView.play { (true) in
+            animationView.removeFromSuperview()
+        }
+        
+        animationView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        animationView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        animationView.heightAnchor.constraint(equalToConstant: 250).isActive = true
     }
-
-
+    
 }
 
